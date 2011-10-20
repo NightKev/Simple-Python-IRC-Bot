3f327fd21b94fbca9d24e0fa8a206b9b847f39ec
NightKev <NightKev@Gmail.com>
2011-10-20 01:49:44 -0700

    add file

--- functions/__init__.py
+++ functions/__init__.py
@@ -1,11 +1,1 @@
-The latest information about this project, the Pyra IRC Framework (Python Raw IRC Framework), should be found at:
-http://www.intuit-symbiosis.org/computing or https://sourceforge.net/projects/pyrawirc/ .
-
-*** You need Python to run these files! ***
-
-In this directory should there be two clients:
--- one text client for curses (doesn't work in windows)
--- one gui client using tkinter
--- also one simplistic tkinter client for educative purposes
-
-Edit irc_settings.py with your nickname and server information. Have fun and please be responsible!
+__all__ = []
---
+++ pybot.py
@@ -0,0 +1,2 @@
+from ircutils import *
+
