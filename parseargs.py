899ab73f883294a84c678ef28e34014fdfcf3341
Kevin Skusek <nightkev@gmail.com>
2012-06-23 04:29:21 -0700

    on second thought, I shouldn't be versioning that file after all

--- parseargs.py
+++ parseargs.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2011 Kevin Skusek
+# Copyright (c) 2011,2012 Kevin Skusek
 # The full copyright notice can be found in the file LICENSE
 
 def getargs():
@@ -6,7 +6,7 @@ def getargs():
 
     parser = argparse.ArgumentParser(description='Start up the IRC bot.')
     parser.add_argument('-n','--nick', required=True, help="The nickname of the bot. Required.")
-    parser.add_argument('-p','--password', default='', help="The bot's nickserv pass (only useful if the IRC server has nickname registration services).")
+    parser.add_argument('-p','--password', default='', help="The bot's nickserv pass (only useful if the IRC server has nickname registration services and you have registered the nickname used by the bot).")
     parser.add_argument('-s','--server', required=True, help="The IRC server to connect to (ex: irc.example.com). Required.")
     parser.add_argument('--port', help="The port to connect to the IRC server on. Defaults to 6667.")
     parser.add_argument('--modes', default='+Bix', help="Initial usermodes to set on the bot upon joining the server. Defaults to '+Bix'.")
---
+++ parseconfig.py
@@ -0,0 +1,4 @@
+# Copyright (c) 2011,2012 Kevin Skusek
+# The full copyright notice can be found in the file LICENSE
+
+import ConfigParser
